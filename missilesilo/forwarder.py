827af375572956d499801a7f301f0d5762c341a1
Scott Evtuch <scott@evtuch.net>
2020-08-23 12:55:00 -0500

    Add some output for debugging

--- missilesilo/forwarder.py
+++ missilesilo/forwarder.py
@@ -3,12 +3,15 @@ import socket
 class Forwarder():
     def invoke(self, data, timeout=1):
         # Connect to the socket
+        print('Connecting to ssh-agent socket at {}'.format(self.filepath))
         my_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         my_socket.connect(self.filepath)
 
+        print('Sending data to ssh-agent')
         my_socket.send(data)
 
         # Receive data sent to us
+        print('Attempting to receive data from ssh-agent')
         my_socket.settimeout(timeout)
         received_data = b''
         try:
@@ -16,15 +19,19 @@ class Forwarder():
             while len(received_data) < 4:
                 received_data += my_socket.recv(1024)
             expected_length = int.from_bytes(received_data[0:4],"big")
+            print('Header received for {} length packet from ssh-agent'.format(expected_length))
             while len(received_data) - 4 < expected_length:
                 # Keep receiving data until we get it all
                 received_data += my_socket.recv(1024)
             # We've got all the data, return it
+            print('Full packet received from ssh-agent')
         except socket.timeout:
-            # Socket timed out, move along
-            pass
+            # Socket timed out, we didn't receive the full response
+            print('ssh-agent socket timed out, something went wrong')
+            raise
         
         # Close the socket
+        print('Closing ssh-agent socket')
         my_socket.close()
         
         return received_data
--- missilesilo/listener.py
+++ missilesilo/listener.py
@@ -11,6 +11,7 @@ class Listener():
                 raise
         
         # Create a socket and start listening
+        print('Binding to {} and listening'.format(self.filepath))
         self.my_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.my_socket.bind(self.filepath)
         self.my_socket.listen(1)
@@ -19,9 +20,11 @@ class Listener():
         while True:
             # Accept connections to my socket
             if None == self.my_connection:
+                print('Waiting for a connection from the client')
                 self.my_connection, client_address = self.my_socket.accept()
 
             # Receive data sent to us
+            print('Attempting to receive data from the client')
             self.my_connection.settimeout(timeout)
             received_data = b''
             try:
@@ -34,13 +37,16 @@ class Listener():
                         # The client has probably left us
                         raise socket.timeout
                 expected_length = int.from_bytes(received_data[0:4],"big")
+                print('Header received for {} length packet from client'.format(expected_length))
                 while len(received_data) - 4 < expected_length:
                     # Keep receiving data until we get it all
                     received_data += self.my_connection.recv(1024)
                 # We've got all the data, return it
+                print('Full packet received from client')
                 break
             except socket.timeout:
                 # Socket timed out, close the connection and move along
+                print('Client connection timed out, closing connection')
                 self.my_connection.close()
                 self.my_connection = None
             
@@ -48,6 +54,7 @@ class Listener():
     
     def respond(self, data):
         # Send the data to the socket
+        print('Sending data to the client')
         self.my_connection.send(data)
 
     def __init__(self, filepath="/tmp/missilesilo"):
